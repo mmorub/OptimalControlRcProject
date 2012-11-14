e247f8485cc3a5ec6a221450345a2918d255a299
moennigmann <martin.moennigmann@rub.de>
2012-11-14 19:02:07 +0100

    First commit. Code has been moved from ArduinoSender (unused here, used to be option) to Snippets/arduino (in order not to delete the code altogether).

--- ArduinoSender/arduino/arduinoSender/arduinoSender.ino
+++ ArduinoSender/arduino/arduinoSender/arduinoSender.ino
@@ -187,30 +187,6 @@ inline void adjust_steering_signal(){
 }
 
 /**
- * Set timer 2 for interrupts at 125kHz.  
- */
-void setup_interrupt_125kHz(){
-  cli(); // stop interrupts
-
-  TCCR2A = 0; // set TCCR2A register to 0
-  TCCR2B = 0; // set TCCR2B register to 0 
-  TCNT2  = 0; // initialize counter value to 0
-
-  // set interrupt interrupt_frequency to 125kHz by setting
-  // prescaler to 8 and counter to 15 for
-  // 16MHz/prescaler/(counter+ 1)= 16MHz/8/16= 125kHz
-  OCR2A = 15; 
-  // turn on CTC mode
-  TCCR2A |= (1 << WGM21);
-  // set prescaler to 8
-  TCCR2B |= (1<< CS11); 
-  // enable timer compare interrupt
-  TIMSK2 |= (1 << OCIE2A);
-
-  sei(); // enable interrupts
-}
-
-/**
  * Set timer 2 for interrupts at 100kHz.  
  */
 void setup_interrupt2_100kHz(){
@@ -234,57 +210,5 @@ void setup_interrupt2_100kHz(){
   sei(); // enable interrupts
 }
 
-/**
- * Set timer 2 for interrupts at 80kHz.   
- */
-void setup_interrupt_80kHz(){
-  cli(); // stop interrupts
 
-  TCCR2A = 0; // set TCCR2A register to 0
-  TCCR2B = 0; // set TCCR2B register to 0 
-  TCNT2  = 0; // initialize counter value to 0
-
-  // set interrupt_frequency to 80kHz by setting
-  // prescaler to 8 and counter to 24 for
-  // 16MHz/prescaler/(counter+ 1)= 16MHz/8/25= 80kHz
-  OCR2A = 24; 
-  // turn on CTC mode
-  TCCR2A |= (1 << WGM21);
-  // set prescaler to 8
-  TCCR2B |= (1<< CS11); 
-  // enable timer compare interrupt
-  TIMSK2 |= (1 << OCIE2A);
-
-  // set prescaler to 64
-  //TCCR2B |= (1 << CS10) | (1 << CS11);   
-
-  sei(); //allow interrupts
-}
-
-/**
- * Set timer 2 for interrupts at 8kHz.   
- */
-void setup_interrupt_8kHz(){
-  cli(); // stop interrupts
-
-  TCCR2A = 0; // set TCCR2A register to 0
-  TCCR2B = 0; // set TCCR2B register to 0 
-  TCNT2  = 0; // initialize counter value to 0
-
-  // set interrupt_frequency to 8kHz by setting
-  // prescaler to 8 and counter to 249 for
-  // 16MHz/prescaler/(counter+ 1)= 16MHz/8/250= 8kHz
-  OCR2A = 249; 
-  // turn on CTC mode
-  TCCR2A |= (1 << WGM21);
-  // set prescaler to 8
-  TCCR2B |= (1<< CS11); 
-  // enable timer compare interrupt
-  TIMSK2 |= (1 << OCIE2A);
-
-  // set prescaler to 64
-  //TCCR2B |= (1 << CS10) | (1 << CS11);   
-
-  sei(); //allow interrupts
-}
 
