8af0b5b87a858c2f052c4b65da1f3b2611128cd8
moennigmann <martin.moennigmann@rub.de>
2013-02-26 15:47:32 +0100

    Now uses MSB to distinguish between throttle and steering signal.

--- ArduinoSender/tests/loop_steering_angle.c
+++ ArduinoSender/tests/loop_steering_angle.c
@@ -1,49 +1,60 @@
+#include <stdio.h> /* Standard input/output definitions */
+#include <stdlib.h> /* needed for EXIT_SUCCESS */
+#include <stdint.h> /* Standard types, in particular uint8_t */
+#include <fcntl.h> /* File control definitions, in particular O_RDWR etc. */
+#include <errno.h> /* Error number definitions */
+#include <termios.h> /* POSIX terminal control definitions */
 
-#include <stdio.h>    /* Standard input/output definitions */
-#include <stdlib.h>   /* needed for EXIT_SUCCESS */
-#include <stdint.h>   /* Standard types, in particular uint8_t */
-#include <fcntl.h>    /* File control definitions, in particular O_RDWR etc. */
-#include <errno.h>    /* Error number definitions */
-#include <termios.h>  /* POSIX terminal control definitions */
-
-#include "serialport_init.h"      // see Makefile for path to included and linked code
+#include "serialport_init.h" // see Makefile for path to included and linked code
 #include "serialport_writebyte.h" // see Makefile for path to included and linked code
 
 /**
- * Sends a single byte ("current_signal") every 1/10s to the serial port (defined in variable serialport);
- * loops values of byte between bounds set in lower_bound_signal and upper_bound_signal.
- * 
- * Typical pitfalls:
- * - make sure serial port selected here (varialbe "serialport") and on other end are equal
- * - make sure baudrate here and on other end are equal
- * - make sure receiving end can handle values between lower_bound_signal and upper_bound_signal
- * 
- */
-int main(int argc, char *argv[]) 
+* Sends a single byte ("current_signal") every 1/10s to the serial port (defined in variable serialport);
+* loops values of byte between bounds set in lower_bound_signal and upper_bound_signal.
+*
+* Typical pitfalls:
+* - make sure serial port selected here (variable "serialport") and on other end are equal
+* - make sure baudrate here and on other end are equal
+* - make sure receiving end can handle values between lower_bound_signal and upper_bound_signal
+*
+*/
+int main(int argc, char *argv[])
 {
     int fd = 0;
-    char serialport[]= {"/dev/ttyACM3"};  
-    const int baudrate = B9600;  
-    int rc; 
-    uint8_t current_signal= 120;
-    const uint8_t lower_bound_signal= 105;
-    const uint8_t upper_bound_signal= 135;
-    uint8_t delta_signal= 1; 
-    fd = serialport_init(serialport, baudrate);
+    char serialport[]= {"/dev/ttyACM3"};		
+    const int baudrate = B9600;
+    int rc;
+    uint8_t steering_signal= 15;				
+    uint8_t throttle_signal = 30;				
+    uint8_t delta_signal= 1;
+    fd = serialport_init(serialport, baudrate);			// open handle for serialport
     while (1){
-      rc = serialport_writebyte(fd, current_signal);
-      if(rc==-1) return -1;
-      usleep(20000); // sleep 1/10 s
-      
-      if (current_signal>= upper_bound_signal){
-	delta_signal= -1;
-	usleep(2000000);
-      } else if (current_signal<= lower_bound_signal){
-	delta_signal= 1;
-	usleep(2000000); 
+      // if MSB is set to 1, the signal is steering (as opposed to throttle) signal
+      steering_signal |= 1<<7;
+      rc = serialport_writebyte(fd, steering_signal);
+
+      // set MSB back to zero to be able to carry out calculations
+      steering_signal &= ~(1<<7); //set MSB back to '0'
+      if(rc==-1) {
+    	printf("failed to send byte [steering]");
+    	  return -1;
       }
-      current_signal += delta_signal;  
+
+      // send throttle signal
+      rc = serialport_writebyte(fd, throttle_signal);
+      if(rc==-1) {
+    	  printf("failed to send byte [throttle]");
+    	  return -1;
+      }
+      usleep(100000); // sleep 1/10 s
+
+      if (steering_signal>= 30){
+    	  delta_signal= -1;
+      } else if (steering_signal<= 0){
+    	  delta_signal= 1;
+      }
+      steering_signal += delta_signal;
     }
-    exit(EXIT_SUCCESS);    
-} 
-    
+    close(fd);				// close handle for serialport 
+    exit(EXIT_SUCCESS);
+}
